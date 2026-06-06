32cd7cb32e12901f881abad9dfedf4cdaf18738c
Hernan Alvarado <hernanvid123@gmail.com>
2026-06-06 18:59:02 -0500

    feat(blocks): add prebuilt sign-in blocks (#3)

---
+++ packages/registry/src/registry/new-york/blocks/react-router/sign-in/sign-in.tsx
@@ -0,0 +1,117 @@
+"use client"
+
+import { useAuthActions } from "@aura-stack/react-router/client"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import { Field, FieldGroup, FieldLabel, FieldSeparator } from "@/components/ui/field"
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
+import type { FormEvent } from "react"
+
+export const SignIn = () => {
+    const { signIn, signInCredentials, isPending } = useAuthActions()
+
+    const onSubmit = async (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+        const email = formData.get("email") as string
+        const password = formData.get("password") as string
+        await signInCredentials({
+            payload: {
+                username: email,
+                password,
+            },
+        })
+    }
+
+    const onGitlabSignIn = async () => {
+        await signIn("gitlab", { redirect: true })
+    }
+
+    const onGithubSignIn = async () => {
+        await signIn("github", { redirect: true })
+    }
+
+    return (
+        <div className="w-full min-h-dvh max-w-lg mx-auto place-content-center">
+            <Card className="max-w-lg px-6 py-8 sm:p-12 relative gap-6">
+                <CardHeader className="text-center gap-6 p-0">
+                    <div className="flex flex-col gap-1">
+                        <CardTitle className="text-2xl font-medium text-card-foreground">Welcome to Aura Stack</CardTitle>
+                        <CardDescription className="text-sm text-muted-foreground font-normal">
+                            Login to your account now
+                        </CardDescription>
+                    </div>
+                </CardHeader>
+                <CardContent className="p-0">
+                    <form onSubmit={onSubmit}>
+                        <FieldGroup className="gap-6">
+                            <Field className="grid md:grid-cols-2 md:gap-6 gap-3">
+                                <Button
+                                    variant="outline"
+                                    type="button"
+                                    className="text-sm text-medium text-card-foreground gap-2 dark:bg-background rounded-lg h-9 shadow-xs cursor-pointer"
+                                    disabled={isPending}
+                                    onClick={onGitlabSignIn}
+                                >
+                                    Sign in with Gitlab
+                                </Button>
+                                <Button
+                                    variant="outline"
+                                    type="button"
+                                    className="text-sm text-medium text-card-foreground gap-2 dark:bg-background rounded-lg h-9 shadow-xs cursor-pointer"
+                                    disabled={isPending}
+                                    onClick={onGithubSignIn}
+                                >
+                                    Sign in with Github
+                                </Button>
+                            </Field>
+                            <FieldSeparator className="*:data-[slot=field-separator-content]:bg-card text-sm text-muted-foreground bg-transparent">
+                                <span className="px-4">or sign in with</span>
+                            </FieldSeparator>
+                            <div className="flex flex-col gap-4">
+                                <Field className="gap-1.5">
+                                    <FieldLabel htmlFor="email" className="text-sm text-muted-foreground font-normal">
+                                        Email*
+                                    </FieldLabel>
+                                    <Input
+                                        id="email"
+                                        type="email"
+                                        name="email"
+                                        placeholder="example@shadcnspace.com"
+                                        required
+                                        className="dark:bg-background h-9 shadow-xs"
+                                    />
+                                </Field>
+                                <Field className="gap-1.5">
+                                    <FieldLabel htmlFor="password" className="text-sm text-muted-foreground font-normal">
+                                        Password*
+                                    </FieldLabel>
+                                    <Input
+                                        id="password"
+                                        type="password"
+                                        name="password"
+                                        placeholder="Enter your password"
+                                        required
+                                        className="dark:bg-background h-9 shadow-xs"
+                                    />
+                                </Field>
+                            </div>
+                            <Field className="gap-4">
+                                <Button
+                                    type="submit"
+                                    size="lg"
+                                    className="rounded-lg h-10 hover:bg-primary/80 cursor-pointer"
+                                    disabled={isPending}
+                                >
+                                    Sign in
+                                </Button>
+                            </Field>
+                        </FieldGroup>
+                    </form>
+                </CardContent>
+            </Card>
+        </div>
+    )
+}
+
+export default SignIn
