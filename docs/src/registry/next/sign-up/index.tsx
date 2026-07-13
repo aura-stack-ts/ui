caa08b666baa13280b34f51a2ec7c14392c5c255
Hernan Alvarado <hernanvid123@gmail.com>
2026-07-13 15:52:39 -0500

    feat(registry): add `sign-up-2` authentication blocks (#12)

--- docs/src/registry/next/sign-up/index.tsx
+++ docs/src/registry/next/sign-up/index.tsx
@@ -1,1 +1,2 @@
 export { SignUp as NextSignUp1 } from "@/registry/next/sign-up/sign-up"
+export { SignUp as NextSignUp2 } from "@/registry/next/sign-up/sign-up-2"
--- docs/src/registry/react-router/sign-up/index.tsx
+++ docs/src/registry/react-router/sign-up/index.tsx
@@ -1,1 +1,2 @@
 export { SignUp as ReactRouterSignUp1 } from "@/registry/react-router/sign-up/sign-up"
+export { SignUp as ReactRouterSignUp2 } from "@/registry/react-router/sign-up/sign-up-2"
