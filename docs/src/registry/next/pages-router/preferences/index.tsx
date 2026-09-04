dc53b548bf5da544b197efc8229951de87072652
Hernan Alvarado <hernanvid123@gmail.com>
2026-09-04 08:21:40 -0500

    feat(registry): add `preferences-2` blocks

--- docs/src/registry/next/pages-router/preferences/index.tsx
+++ docs/src/registry/next/pages-router/preferences/index.tsx
@@ -1,1 +1,2 @@
 export { Preferences as PreferencesNextPages1 } from "@/registry/next/pages-router/preferences/preferences"
+export { Preferences as PreferencesNextPages2 } from "@/registry/next/pages-router/preferences/preferences-2"
--- docs/src/registry/react/preferences/index.tsx
+++ docs/src/registry/react/preferences/index.tsx
@@ -1,1 +1,2 @@
 export { Preferences as PreferencesReact1 } from "@/registry/react/preferences/preferences"
+export { Preferences as PreferencesReact2 } from "@/registry/react/preferences/preferences-2"
