5c1d355a8c38365dac0c43b791cc8e5881560f10
Hernan Alvarado <hernanvid123@gmail.com>
2026-09-05 14:13:12 -0500

    feat(registry): add `integrations-3` blocks (#31)
    * feat(registry): add `integrations-3` blocks
    
    * chore: apply coderabbit

--- docs/src/registry/react-router/integrations/index.tsx
+++ docs/src/registry/react-router/integrations/index.tsx
@@ -1,2 +1,3 @@
 export { Integrations as IntegrationsReactRouter1 } from "@/registry/react-router/integrations/integrations"
 export { Integrations as IntegrationsReactRouter2 } from "@/registry/react-router/integrations/integrations-2"
+export { Integrations as IntegrationsReactRouter3 } from "@/registry/react-router/integrations/integrations-3"
