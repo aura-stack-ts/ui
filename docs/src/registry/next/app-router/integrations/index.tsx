958d1964cad07ff0ce44319afc22d912a7888b11
Hernan Alvarado <hernanvid123@gmail.com>
2026-09-05 15:41:45 -0500

    feat(registry): add `integrations-4` blocks

--- docs/src/registry/next/app-router/integrations/index.tsx
+++ docs/src/registry/next/app-router/integrations/index.tsx
@@ -1,3 +1,4 @@
 export { Integrations as IntegrationsNextApp1 } from "@/registry/next/app-router/integrations/integrations"
 export { Integrations as IntegrationsNextApp2 } from "@/registry/next/app-router/integrations/integrations-2"
 export { Integrations as IntegrationsNextApp3 } from "@/registry/next/app-router/integrations/integrations-3"
+export { Integrations as IntegrationsNextApp4 } from "@/registry/next/app-router/integrations/integrations-4"
